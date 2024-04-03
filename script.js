1b86defa671d8a6fc5163693e2c3c5235fc87429
Satoo <110577684+BeixuanTianjun@users.noreply.github.com>
2024-04-03 11:35:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function Login(){
+    let username = document.getElementById("username").Value;
+    let password = document.getElementById("password").Value;
+    if(username == "admin" && password == "admin"){
+        window.alert("Login Berhasil");
+        window.location("CV.html");
+        
+        }
+    }
+    
